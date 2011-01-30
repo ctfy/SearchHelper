3e2833758bf154c0ee96e8a51252f91969962bb4
ChengTuFeiYang <mg5812@gmail.com>
2011-01-30 18:36:21 +0800

    REV: 一些零碎的改动

--- Client/Model/Article.cs
+++ Client/Model/Article.cs
@@ -29,20 +29,7 @@ namespace ITJZ.SearchHelper.Client.Model
 
         internal static Article[] getArticleList(Category CurrentCategory)
         {
-            List<Article> list= new List<Article>();
-            for (int i = 0; i < 10; i++)
-            {
-                list.Add(new Article()
-                {
-                    Uid="user-guid-1",
-                    CategoryGuid="category-guid-1",
-                    Guid="article-guid-" +i,
-                    Title="文章" + i,
-                    Content="内容" +i,
-                    CreateTime=DateTime.Now
-                });
-            }
-            return list.ToArray();
+            return DataServer.DataServer.getInstance().Search("", CurrentCategory.Guid).ToArray();
         }
 
         public override string ToString()
